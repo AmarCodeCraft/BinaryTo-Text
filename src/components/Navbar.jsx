eddff633ad821a4328ed6a6f95a1ff9ea1adaada
Amarnath WebDev <amarnathkumar2021@gmail.com>
2025-05-06 18:26:42 +0530

    some error fixing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <div className='navbar flex justify-between items-center bg-black text-white p-4'>
-        <h1 className='text-3xl font-bold'>Binrary To Text</h1>
+        <h1 className='text-3xl font-bold'>Binary To Text</h1>
         <ul className='flex px-2 gap-4 font-semibold'>
             <li className='px-5 py-2 font-semibold hover:text-gray-200 cursor-pointer'>Home</li>
             <li className='px-5 py-2 font-semibold hover:text-gray-200 cursor-pointer'>About</li>
